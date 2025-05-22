bba24bfa0a045e4cee43a7213af83095b2ac910e
Allison Codi <allison.codi@emory.edu>
2025-05-22 15:30:11 -0400

    job name drotr

--- sub_jobs.sh
+++ sub_jobs.sh
@@ -6,5 +6,5 @@ sbatch --array=1-$NSEEDS \
        --partition=$PARTITION \
        -n 1 \
        --output=/projects/dbenkes/allison/drotr_example_analysis/scratch/%a_%J.out \
-       --job-name=seed_%a \
+       --job-name=drotr \
        --wrap "Rscript run_analysis.R \$SLURM_ARRAY_TASK_ID $NUISANCE"
